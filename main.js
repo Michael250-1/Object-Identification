8238cb6846145a36338fa7ba7f6ad3b4fa1bda2b
Michael250-1 <79066678+Michael250-1@users.noreply.github.com>
2021-12-10 16:57:36 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status="";
 img="";
+objects=[];
 function setup(){
     canvas=createCanvas(640,420);
     canvas.center();
@@ -16,21 +17,27 @@ if (error){
     console.log(error);
 }
 console.log(results);
+objects=results;
 }
 function preload(){
     img=loadImage("dog_cat.jpg");
 }
 function draw(){
     image(img,0,0,640,420);
-    fill("#FF0000");
-    text("Dog",50,80);
-    noFill();
-    stroke("#FF0000");
-    rect(30,60,450,300);
-
-    fill("#FF0000");
-    text("Cat",300,115);
-    noFill();
-    stroke("#FF0000");
-    rect(280,90,300,300);
+   
+    if (status != ""){
+        document.getElementById("status").innerHTML="status : Object Detected";
+        for(i=0;i<objects.length;i++){
+            fill("#FF0000");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+ " "+ percent+ "%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    
+   
+  
+    
+    }
 }
